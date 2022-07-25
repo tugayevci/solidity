217225a70f941471c9fd509839951882b4a2597e
Tugay Cem Evci <tugay.evci@crowdsol.com>
2022-07-25 16:32:54 +0300

    fix: ether import fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,18 @@ import Button from "react-bootstrap/Button";
 import { ethers } from "ethers";
 import MyContract from "../artifacts/contracts/MyContract.sol/MyContract.json";
 
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-// get the end user
-const signer = provider.getSigner();
-
-// get the smart contract
-const contract = new ethers.Contract(contractAddress, MyContract.abi, signer);
-
 export default function Home() {
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+  const provider = ethers && new ethers.providers.Web3Provider(window.ethereum);
+
+  // get the end user
+  const signer = provider && provider.getSigner();
+
+  // get the smart contract
+  const contract =
+    ethers && new ethers.Contract(contractAddress, MyContract.abi, signer);
+
   const [name, setName] = useState("");
   const [contractName, setContractName] = useState("");
   const [account, setAccount] = useState(null);
--- src/pages/Install.jsx
+++ src/pages/Install.jsx
@@ -1,5 +1,4 @@
 import metamask from "../assets/metamask.png";
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 const Install = () => {
